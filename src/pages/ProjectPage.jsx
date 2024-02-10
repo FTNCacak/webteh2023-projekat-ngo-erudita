a31bd505ff2e60d8292ecb8f4a07a173c26ddd9f
zer4kaii <57032341+zer4kaii@users.noreply.github.com>
2024-02-10 13:26:06 +0100

    Implementation of fetch logic into projects page

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -5,44 +5,45 @@ import useHttpGet from "../Hooks/useHttpGet";
 import { useEffect } from "react";
 
 const ProjectPage = () => {
+  const fetchData = useHttpGet("projects");
 
-  const fetchData = useHttpGet('projects');
-
-  useEffect(()=>{
-    if(fetchData.error==null && fetchData.loading == false)
-        console.log(fetchData.data);
+  useEffect(() => {
+    if (fetchData.error === null && fetchData.loading === false)
+      console.log(fetchData.data);
   }, []);
 
   return (
     <>
       <div className={styles.projectContainer}>
         <h1 className={styles.mainHeader}>Projekti</h1>
-        <Row className={styles.projectDiv}>
-          <Col md={4} sm={6} xs={12} className={styles.textCol}>
-            <div className={styles.textProject}>
-              <h3>Srusimo 4 zida</h3>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
-                animi suscipit quibusdam nesciunt non sit hic voluptatem
-                cupiditate labore optio atque eum autem minima voluptatum
-                voluptate amet numquam, libero tenetur?
-              </p>
-              <Link to={"/o-nama"}>Saznajte više</Link>
-            </div>
-          </Col>
-          <Col md={8} sm={6} xs={12}>
-            <div className={styles.contentProject}>
-              <img
-                alt='projekat'
-                src={
-                  process.env.PUBLIC_URL +
-                  "/imgs/projects/Srusimo-4-zida-photo-1.png"
-                }
-              ></img>
-            </div>
-          </Col>
-        </Row>
-      </div>
+
+        
+        {
+          //Koristena je mapa da bi se proslo kroz sve elemente baze projekti
+          //Znak pitanja je osiguranje da ne baca error dok se data ne ucita
+          fetchData.data?.map((data) => {
+            return (
+                <Row className={styles.projectDiv} key={data.id}>
+                  <Col md={4} sm={6} xs={12} className={styles.textCol}>
+                    <div className={styles.textProject}>
+                      <h3>{data.project_name}</h3>
+                      <p>{data.short_description}</p>
+                      <Link to={"/o-nama"}>Saznajte više</Link>
+                    </div>
+                  </Col>
+                  <Col md={8} sm={6} xs={12}>
+                    <div className={styles.contentProject}>
+                      <img
+                        alt="projekat"
+                        src={process.env.PUBLIC_URL + data.photo}
+                      ></img>
+                    </div>
+                  </Col>
+                </Row>
+            );
+          })
+        }
+        </div>
     </>
   );
 };
