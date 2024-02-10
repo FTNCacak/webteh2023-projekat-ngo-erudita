c3481728b61a154f56a2b672e9c03addcebfc7e6
zer4kaii <57032341+zer4kaii@users.noreply.github.com>
2024-02-10 21:41:23 +0100

    Single project page

--- src/pages/SingleProjectPage.jsx
+++ src/pages/SingleProjectPage.jsx
@@ -5,48 +5,49 @@ import useHttpGet from "../Hooks/useHttpGet";
 import { useEffect, useState } from "react";
 //import ImageSlider from "../compontents/ImageSlider";
 
-
 const SingleProjectPage = () => {
-    const fetchData = useHttpGet("projects");
-    const {projectUrl} = useParams();
+  const fetchData = useHttpGet("projects");
+  const { projectUrl } = useParams();
 
-    //const images = ['./imgs/projects/Srusimo-4-zida-photo-1.jpg', './imgs/projects/Srusimo-4-zida-photo-2.jpg', './imgs/projects/Srusimo-4-zida-photo-3.jpg'];
+  //const images = ['./imgs/projects/Srusimo-4-zida-photo-1.jpg', './imgs/projects/Srusimo-4-zida-photo-2.jpg', './imgs/projects/Srusimo-4-zida-photo-3.jpg'];
 
-    //State koji cuva podatke trenutno izabranog projekta
-    const [selectedData, setSelectedData] = useState();
+  //State koji cuva podatke trenutno izabranog projekta
+  const [selectedData, setSelectedData] = useState();
 
-    useEffect(() => {
-        if (fetchData.error === null && fetchData.loading === false)
-        //provera da li se naziv projekta podudara sa url
-        setSelectedData(fetchData.data.filter(data => {
-          return data.project_name
-          .replaceAll(" ","-").toLowerCase().replaceAll("š","s")
-          .replaceAll("č","c").replaceAll("ć","c").replaceAll("ž","z")
-          .replaceAll("đ","dj") === projectUrl;})[0]);
-          console.log(selectedData)
-    }, [fetchData]);
+  useEffect(() => {
+    if (fetchData.error === null && fetchData.loading === false)
+      //provera da li se naziv projekta podudara sa url
+      setSelectedData(
+        fetchData.data.filter((data) => {
+          return (
+            data.project_name
+              .replaceAll(" ", "-")
+              .toLowerCase()
+              .replaceAll("š", "s")
+              .replaceAll("č", "c")
+              .replaceAll("ć", "c")
+              .replaceAll("ž", "z")
+              .replaceAll("đ", "dj") === projectUrl
+          );
+        })[0]
+      );
+    console.log(selectedData);
+  }, [fetchData]);
 
-    
-      return (
-          <Container className={'containerFix'}>
-            <div className={styles.projectContainer}>
-              {
-                <Row className={styles.projectDiv} >
-                  <Col>
-                      <h1>{selectedData?.project_name}</h1>
-                      <img src={process.env.PUBLIC_URL + selectedData?.photo}>
-                      </img>
-                      <p>{selectedData?.long_description}</p>   
-                  </Col>
-                </Row>    
-              }
-              </div> 
+  return (
+    <Container className={"containerFix"}>
+      <div className={styles.projectContainer}>
+        <h1>{selectedData?.project_name}</h1>
+        <div className={styles.imgContainer}>
+          <img src={process.env.PUBLIC_URL + selectedData?.photo}></img>
+        </div>
+        <p>{selectedData?.long_description}</p>
+      </div>
 
-              {/* <div>
+      {/* <div>
                   <ImageSlider images={images} />
               </div>    */}
-              
-          </Container>
-    );
-  };
+    </Container>
+  );
+};
 export default SingleProjectPage;
